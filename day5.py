8b658a4dc2c424ff514ad3b5a800c90e93ab6a14
Jacob Lee <JLpython@outlook.com>
2022-12-05 21:40:30 -0800

    refactor: Day 5

--- day5.py
+++ day5.py
@@ -1,5 +1,6 @@
 # 2022 Day 5
 
+import collections
 import re
 
 
@@ -10,21 +11,21 @@ with open(TXT_PATH, "r", encoding="utf-8") as file:
     DATA = file.readlines()
 
 
-def get_columns(arr, a: int = None, b: int = None):
+def get_columns(arr: list[str], a: int = None, b: int = None):
     crates = [
-        [x[i + 1] for i in range(0, len(x), 4)]
+        [x[i + 1].strip() for i in range(0, len(x), 4)]
         for x in arr.__getitem__(slice(a, b))
     ]
     stacks = [
-        [crates[i][j] for i in range(len(crates))]
+        reversed([crates[i][j] for i in range(len(crates))])
         for j in range(len(crates[0]))
     ]
-    columns = [[x for x in reversed(c) if x != " "] for c in stacks]
+    columns = [collections.deque(filter(bool, c)) for c in stacks]
 
     return columns
 
 
-def get_moves(arr, a: int = None, b: int = None):
+def get_moves(arr: list[str], a: int = None, b: int = None):
     moves = [
         map(int, re.findall(r"\d+", x))
         for x in arr.__getitem__(slice(a, b))
@@ -33,24 +34,25 @@ def get_moves(arr, a: int = None, b: int = None):
     return moves
 
 
-def part1(arr=DATA):
+def part1(arr: list[str] = DATA):
     columns = get_columns(arr, None, 8)
     moves = get_moves(arr, 10, None)
 
     for n, a, b in moves:
-        columns[b - 1] += reversed(columns[a - 1][-n:])
-        del columns[a - 1][-n:]
+        for _ in range(n):
+            columns[b - 1].append(columns[a - 1].pop())
 
     return "".join(x[-1] for x in columns if x)
 
 
-def part2(arr=DATA):
+def part2(arr: list[str] = DATA):
     columns = get_columns(arr, None, 8)
     moves = get_moves(arr, 10, None)
 
     for n, a, b in moves:
-        columns[b - 1] += columns[a - 1][-n:]
-        del columns[a - 1][-n:]
+        columns[a - 1].rotate(n)
+        for _ in range(n):
+            columns[b - 1].append(columns[a - 1].popleft())
 
     return "".join(x[-1] for x in columns if x)
 
