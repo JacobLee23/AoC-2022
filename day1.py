a3ca406ae61a8a77f246142646e25f0b9235e848
Jacob Lee <JLpython@outlook.com>
2022-12-07 12:12:05 -0800

    refactor: Day 1

--- day1.py
+++ day1.py
@@ -1,23 +1,41 @@
 # 2022 Day 1
 
-
-TXT_PATH = "day1.txt"
-
-
-with open(TXT_PATH, "r", encoding="utf-8") as file:
-    DATA = [list(map(int, x.split())) for x in file.read().split("\n\n")]
+import typing
 
 
-def part1(arr=DATA):
+# Environment constants
+DATA_PATH = {
+    "puzzle": "day1.txt",
+    "test": "day1-test.txt"
+}
+DATA = {
+    "puzzle": None,
+    "test": None
+}
+for key, value in DATA_PATH.items():
+    with open(value, "r", encoding="utf-8") as file:
+        data = [list(map(int, x.split())) for x in file.read().split("\n\n")]
+    DATA[key] = data
+
+
+# Puzzle constants
+N = 3
+
+
+# Part 1 solution
+def part1(arr: typing.List[typing.List[int]] = DATA) -> int:
     return max(list(map(sum, arr)))
 
 
-
-def part2(arr=DATA):
-    return sum(sorted(list(map(sum, arr)))[-3:])
-
+# Part 2 solution
+def part2(arr: typing.List[typing.List[int]] = DATA) -> int:
+    return sum(sorted(list(map(sum, arr)))[-N:])
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print("Test data:")
+    print(f"\tPart 1:\t{part1(DATA['test'])}")
+    print(f"\tPart 2:\t{part2(DATA['test'])}")
+    print("Puzzle data:")
+    print(f"\tPart 1:\t{part1(DATA['puzzle'])}")
+    print(f"\tPart 2:\t{part2(DATA['puzzle'])}")
