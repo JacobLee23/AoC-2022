02698d0e12dd265f20525b909b7f312260f9133e
Jacob Lee <JLpython@outlook.com>
2022-12-07 09:50:55 -0800

    feat: update set-up procedure

--- setupnew.py
+++ setupnew.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 import sys
 
@@ -6,56 +5,74 @@ import sys
 YEAR = 2022
 
 
-def new(day: int, txt_path: str) -> pathlib.Path:
-    path = pathlib.Path(f"day{day}.py")
-
-    with open(path, "w", encoding="utf-8") as file:
+def py_template(day: int, py_path: str, data_path: str, test_path: str) -> None:
+    with open(py_path, "w", encoding="utf-8") as file:
         file.write(
             f"""# {YEAR} Day {day}
 
-
-TXT_PATH = "{txt_path}"
-
-
-with open(TXT_PATH, "r", encoding="utf-8") as file:
-    DATA = file.readlines()
+import typing
 
 
-def part1(arr=DATA):
+# Environment constants
+DATA_PATH = {{
+    "puzzle": "{data_path}",
+    "test": "{test_path}"
+}}
+DATA = {{
+    "puzzle": None,
+    "test": None
+}}
+
+for key, value in DATA_PATH.ITEMS():
+    with open(value, "r", encoding="utf-8") as file:
+        data = file.readlines()
+    DATA[key] = data
+
+
+# Puzzle constants
+
+
+# Part 1 solution
+def part1(arr: typing.List[str] = DATA) -> typing.Any:
     pass
 
 
-def part2(arr=DATA):
+# Part 2 solution
+def part2(arr: typing.List[str] = DATA) -> typing.Any:
     pass
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print("Test data:")
+    print(f"\\tPart 1:\\t{{part1(DATA['test'])}}")
+    print(f"\\tPart 2:\\t{{part2(DATA['test'])}}")
+    print("Puzzle data:")
+    print(f"\\tPart 1:\\t{{part1(DATA['puzzle'])}}")
+    print(f"\\tPart 2:\\t{{part2(DATA['puzzle'])}}")
 """
         )
-
-    return path
 
 
 def main(*args):
     day = int(args[0])
 
     # Construct file paths
-    txt_path = pathlib.Path(f"day{day}.txt")
     py_path = pathlib.Path(f"day{day}.py")
+    data_path = pathlib.Path(f"day{day}.txt")
+    test_path = pathlib.Path(f"day{day}-test.txt")
+    paths = [data_path, test_path, py_path]
 
     # Verify that both file do not already exists
-    if txt_path.exists() and py_path.exists():
-        raise os.error("[Errno 17] File exists")
+    for path in paths:
+        if path.exists():
+            continue
+        
+        open(path, "w", encoding="utf-8").close()
 
-    # Create .txt file
-    open(txt_path, "w", encoding="utf-8").close()
+        if path == py_path:
+            py_template(day, py_path, data_path, test_path)
 
-    # Create .py file
-    py_path = new(day, txt_path)
-
-    print(py_path, txt_path)
+    print(day, py_path, data_path, test_path)
 
 
 if __name__ == "__main__":
