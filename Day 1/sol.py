2473b008ac241afdc0d0f3cc6f28c0884fe9b69a
Jacob Lee <JLpython@outlook.com>
2022-12-07 17:56:46 -0800

    refactor: Day 1

--- Day 1/sol.py
+++ Day 1/sol.py
@@ -5,13 +5,14 @@ import typing
 
 # Environment constants
 DATA_PATH = {
-    "puzzle": "day1.txt",
-    "test": "day1-test.txt"
+    "puzzle": "puzzle-data.txt",
+    "test": "test-data.txt"
 }
 DATA = {
     "puzzle": None,
     "test": None
 }
+
 for key, value in DATA_PATH.items():
     with open(value, "r", encoding="utf-8") as file:
         data = [list(map(int, x.split())) for x in file.read().split("\n\n")]
@@ -32,10 +33,14 @@ def part2(arr: typing.List[typing.List[int]] = DATA) -> int:
     return sum(sorted(list(map(sum, arr)))[-N:])
 
 
-if __name__ == "__main__":
+def main():
     print("Test data:")
     print(f"\tPart 1:\t{part1(DATA['test'])}")
     print(f"\tPart 2:\t{part2(DATA['test'])}")
     print("Puzzle data:")
     print(f"\tPart 1:\t{part1(DATA['puzzle'])}")
     print(f"\tPart 2:\t{part2(DATA['puzzle'])}")
+
+
+if __name__ == "__main__":
+    main()
