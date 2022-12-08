57aab58ee4f88005b63b7b67b08612c57a3ca372
Jacob Lee <JLpython@outlook.com>
2022-12-07 16:54:09 -0800

    refactor: Day 5

--- day5.py
+++ day5.py
@@ -2,61 +2,66 @@
 
 import collections
 import re
+import typing
 
 
-TXT_PATH = "day5.txt"
+# Environment constants
+DATA_PATH = {
+    "puzzle": "day5.txt",
+    "test": "day5-test.txt"
+}
+DATA = {
+    "puzzle": None,
+    "test": None
+}
+
+for key, value in DATA_PATH.items():
+    with open(value, "r", encoding="utf-8") as file:
+        data = [x.split("\n") for x in file.read().split("\n\n")]
+    DATA[key] = data
 
 
-with open(TXT_PATH, "r", encoding="utf-8") as file:
-    DATA = file.readlines()
+# Puzzle constants
 
 
-def get_columns(arr: list[str], a: int = None, b: int = None):
-    crates = [
-        [x[i + 1].strip() for i in range(0, len(x), 4)]
-        for x in arr.__getitem__(slice(a, b))
-    ]
-    stacks = [
-        reversed([crates[i][j] for i in range(len(crates))])
-        for j in range(len(crates[0]))
-    ]
-    columns = [collections.deque(filter(bool, c)) for c in stacks]
+def get_columns(arr: typing.List[str]) -> typing.List[str]:
+    crates = ((c.strip() for c in x[1::4]) for x in arr[:-1])
+    stacks = (filter(bool, reversed(list(x))) for x in zip(*crates))
 
-    return columns
+    return list(map(collections.deque, stacks))
 
 
-def get_moves(arr: list[str], a: int = None, b: int = None):
-    moves = [
-        map(int, re.findall(r"\d+", x))
-        for x in arr.__getitem__(slice(a, b))
-    ]
-
-    return moves
+def get_moves(arr: typing.List[str]) -> typing.List[str]:
+    return [map(int, re.findall(r"\d+", x)) for x in arr]
 
 
-def part1(arr: list[str] = DATA):
-    columns = get_columns(arr, None, 8)
-    moves = get_moves(arr, 10, None)
+# Part 1 solution
+def part1(arr: typing.List[str] = DATA) -> str:
+    columns = get_columns(arr[0])
+    moves = get_moves(arr[1])
 
     for n, a, b in moves:
-        for _ in range(n):
-            columns[b - 1].append(columns[a - 1].pop())
+        columns[b - 1].extend(columns[a - 1].pop() for _ in range(n))
 
     return "".join(x[-1] for x in columns if x)
 
 
-def part2(arr: list[str] = DATA):
-    columns = get_columns(arr, None, 8)
-    moves = get_moves(arr, 10, None)
+# Part 2 solution
+def part2(arr: typing.List[str] = DATA) -> str:
+    columns = get_columns(arr[0])
+    moves = get_moves(arr[1])
 
     for n, a, b in moves:
         columns[a - 1].rotate(n)
-        for _ in range(n):
-            columns[b - 1].append(columns[a - 1].popleft())
+        columns[b - 1].extend(columns[a - 1].popleft() for _ in range(n))
 
     return "".join(x[-1] for x in columns if x)
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print("Test data:")
+    print(f"\tPart 1:\t{part1(DATA['test'])}")
+    print(f"\tPart 2:\t{part2(DATA['test'])}")
+    print("Puzzle data:")
+    print(f"\tPart 1:\t{part1(DATA['puzzle'])}")
+    print(f"\tPart 2:\t{part2(DATA['puzzle'])}")
